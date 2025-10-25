3a1c6436e527981ce7c9608edee69840df6dbb0a
valderasea <157552843+valderasea@users.noreply.github.com>
2025-10-25 18:55:54 +0700

    Add files via upload

---
+++ LOADER/main.lua
@@ -0,0 +1,185 @@
+local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
+
+local Window = Rayfield:CreateWindow({
+    Name = "ValL | Script Loader",
+    LoadingTitle = "Created By Valdera",
+    LoadingSubtitle = "Jelek ya maap",
+    Theme = "Amethyst",
+})
+
+
+local ScriptTab = Window:CreateTab("List Scripts", 4483362458)
+ScriptTab:CreateSection("⭕ JELEK WAJAR GRATIS🗿")
+
+ScriptTab:CreateButton({
+    Name = " MOUNT FUNNY",
+    Callback = function()
+        Rayfield:Notify({Title="OTW", Image="file", Content="Execute MOUNT FUNNY...", Duration=4})
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/valderasea/rosblog/refs/heads/main/MOUNT-FUNNY/main.lua"))()
+    end
+})
+
+ScriptTab:CreateButton({
+    Name = " MOUNT YAHAYUK ( MANUAL )",
+    Callback = function()
+        Rayfield:Notify({Title="OTW", Image="file", Content="Execute MOUNT YAHAYUK...", Duration=4})
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/valderasea/rosblog/refs/heads/main/MOUNT-YAHAYUK/main.lua"))()
+    end
+})
+
+ScriptTab:CreateButton({
+    Name = " MOUNT AGE",
+    Callback = function()
+        Rayfield:Notify({Title="OTW", Image="file", Content="Loading MOUNT AGE...", Duration=4})
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/valderasea/rosblog/refs/heads/main/MOUNT-AGE/main.lua"))()
+    end
+})
+
+ScriptTab:CreateButton({
+    Name = " MOUNT SIBUATAN",
+    Callback = function()
+        Rayfield:Notify({Title="OTW", Image="file", Content="Loading MOUNT SIBUATAN...", Duration=4})
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/valderasea/rosblog/refs/heads/main/MOUNT-SIBUATAN/main.lua"))()
+    end
+})
+
+ScriptTab:CreateButton({
+    Name = " MOUNT ANJ",
+    Callback = function()
+        Rayfield:Notify({Title="OTW", Image="file", Content="Loading MOUNT ANJ...", Duration=4})
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/valderasea/rosblog/refs/heads/main/MOUNT-ANJ/main.lua"))()
+    end
+})
+
+ScriptTab:CreateButton({
+    Name = " KOTA BUKAN GUNUNG",
+    Callback = function()
+        Rayfield:Notify({Title="OTW", Image="file", Content="Loading KOTA BUKAN GUNUNG...", Duration=4})
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/valderasea/rosblog/refs/heads/main/KOTA-BUKAN-GUNUNG/main.lua"))()
+    end
+})
+
+ScriptTab:CreateButton({
+    Name = " MOUNT PENGANGGURAN",
+    Callback = function()
+        Rayfield:Notify({Title="OTW", Image="file", Content="Loading MOUNT PENGANGGURAN...", Duration=4})
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/valderasea/rosblog/refs/heads/main/MOUNT-PENGANGGURAN/main.lua"))()
+    end
+})
+
+ScriptTab:CreateButton({
+    Name = " MOUNT ATIN",
+    Callback = function()
+        Rayfield:Notify({Title="OTW", Image="file", Content="Loading MOUNT ATIN...", Duration=4})
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/valderasea/rosblog/refs/heads/main/MOUNT-ATIN/main.lua"))()
+    end
+})
+
+ScriptTab:CreateButton({
+    Name = " MOUNT ARUNIKA",
+    Callback = function()
+        Rayfield:Notify({Title="OTW", Image="file", Content="Loading MOUNT ARUNIKA...", Duration=4})
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/valderasea/rosblog/refs/heads/main/MOUNT-ARUNIKA/main.lua"))()
+    end
+})
+
+ScriptTab:CreateButton({
+    Name = " MOUNT DAUN",
+    Callback = function()
+        Rayfield:Notify({Title="OTW", Image="file", Content="Loading MOUNT DAUN...", Duration=4})
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/valderasea/rosblog/refs/heads/main/MOUNT-DAUN/main.lua"))()
+    end
+})
+
+ScriptTab:CreateButton({
+    Name = " MOUNT HMMM",
+    Callback = function()
+        Rayfield:Notify({Title="OTW", Image="file", Content="Loading MOUNT HMMM...", Duration=4})
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/valderasea/rosblog/refs/heads/main/MOUNT-HMMM/main.lua"))()
+    end
+})
+
+ScriptTab:CreateButton({
+    Name = " MOUNT PARGOY",
+    Callback = function()
+        Rayfield:Notify({Title="OTW", Image="file", Content="Loading MOUNT PARGOY...", Duration=4})
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/valderasea/rosblog/refs/heads/main/MOUNT-PARGOY/main.lua"))()
+    end
+})
+
+ScriptTab:CreateButton({
+    Name = " MOUNT YACAPE",
+    Callback = function()
+        Rayfield:Notify({Title="OTW", Image="file", Content="Loading MOUNT YACAPE...", Duration=4})
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/valderasea/rosblog/refs/heads/main/MOUNT-YACAPE/main.lua"))()
+    end
+})
+
+ScriptTab:CreateButton({
+    Name = " MOUNT YNTKTS",
+    Callback = function()
+        Rayfield:Notify({Title="OTW", Image="file", Content="Loading MOUNT YNTKTS...", Duration=4})
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/valderasea/rosblog/refs/heads/main/MOUNT-YNTKTS/main.lua"))()
+    end
+})
+
+ScriptTab:CreateButton({
+    Name = " MOUNT NIGHTMARE EXPEDITION",
+    Callback = function()
+        Rayfield:Notify({Title="OTW", Image="file", Content="Loading MOUNT KAWAH EXPEDITION...", Duration=4})
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/valderasea/rosblog/refs/heads/main/MOUNT-NIGHTMARE-EXPEDITION/main.lua"))()
+    end
+})
+
+ScriptTab:CreateButton({
+    Name = " MOUNT BILEK",
+    Callback = function()
+        Rayfield:Notify({Title="OTW", Image="file", Content="Loading MOUNT BILEK...", Duration=4})
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/valderasea/rosblog/refs/heads/main/MOUNT-BILEK/main.lua"))()
+    end
+})
+
+ScriptTab:CreateButton({
+    Name = " MOUNT YUKARI",
+    Callback = function()
+        Rayfield:Notify({Title="OTW", Image="file", Content="Loading Script...", Duration=4})
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/valderasea/rosblog/refs/heads/main/MOUNT-YUKARI/main.lua"))()
+    end
+})
+
+local ScriptTab = Window:CreateTab("Script lain", 4483362458)
+ScriptTab:CreateSection("⭕ Punya orang gtw work kg")
+
+ScriptTab:CreateButton({
+    Name = " ANIM NO VISUAL",
+    Callback = function()
+        Rayfield:Notify({Title="OTW", Image="file", Content="Loading Sc Anim...", Duration=4})
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/valderasea/rosblog/refs/heads/main/EMOTE/main.lua"))()
+    end
+})
+
+ScriptTab:CreateButton({
+    Name = " INFINITE YIELD",
+    Callback = function()
+        Rayfield:Notify({Title="OTW", Image="file", Content="Loading Sc Admin...", Duration=4})
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/valderasea/rosblog/refs/heads/main/ADMIN/main.lua"))()
+    end
+})
+
+ScriptTab:CreateButton({
+    Name = " VIOLENCE DISTRICT ( YG ORG )",
+    Callback = function()
+        Rayfield:Notify({Title="OTW", Image="file", Content="Loading VD...", Duration=4})
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/valderasea/rosblog/refs/heads/main/VDISTRCT/main.lua"))()
+    end
+})
+
+
+
+ScriptTab:CreateButton({
+    Name = " PRIVATE SERVER",
+    Callback = function()
+        Rayfield:Notify({Title="OTW", Image="file", Content="Loading Map...", Duration=4})
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/valderasea/rosblog/refs/heads/main/PRIV-SRV/main.lua"))()
+    end
+})
